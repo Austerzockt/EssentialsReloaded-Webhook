f5f0d5b26ee7f7d4b484f0992d784c0e58803fc4
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2021-04-17 16:48:31 +0200

    Initial Commit for this plugin with Neovim Project Building included

--- src/main/java/io/github/austerzockt/essentialsreloaded/essentialsreloadedwebhook/EssentialsReloadedWebhook.java
+++ src/main/java/io/github/austerzockt/essentialsreloaded/essentialsreloadedwebhook/EssentialsReloadedWebhook.java
@@ -1,12 +1,11 @@
 package io.github.austerzockt.essentialsreloaded.essentialsreloadedwebhook;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
+import io.github.austerzockt.essentialsreloaded.EssentialsReloaded;
 public final class EssentialsReloadedWebhook extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
